8d0c1fc418b35e701c74f931e4d7d899c0ca73e9
afilipch <afilipch@a58d7b47-41b6-4d21-bc94-2c2c30e24b6a>
2015-02-23 16:44:08 +0000

    scripts have to be further documented
    git-svn-id: file:///data/BIO2/andrei/svn/repos/nrlbio/trunk@81 a58d7b47-41b6-4d21-bc94-2c2c30e24b6a

--- advanced/get_mirna_target.py
+++ advanced/get_mirna_target.py
@@ -22,7 +22,7 @@ from nrlbio.pybedtools_extension import interval2seq
 parser = argparse.ArgumentParser(description='produces old-fashion interaction.bed file of mirna and their targets');
 parser.add_argument('path', metavar = 'N', nargs = '?', type = str, help = "path to the new-style interaction file");
 parser.add_argument('-m', '--mirna', nargs = '?', required = True, type = str, help = "path to miRNA fasta file");
-parser.add_argument('-s', '--system', nargs = '?', required = True, choices = ['ce6', 'mm9', 'hg19'], type = str, help = "genome ce6|mm9|hg19");
+parser.add_argument('-f', '--fasta', nargs = '?', required = True, type = str, help = "path to reference(genome) fasta file");
 args = parser.parse_args();
 
 #exec("from sequence_data.systems import %s as gsys" % args.system);
